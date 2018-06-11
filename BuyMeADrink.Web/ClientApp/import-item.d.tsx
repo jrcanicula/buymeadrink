963173b518ee9e79f199f240226858bdfe286081
jrcanicula <jaime.canicula@gmail.com>
2018-06-11 20:12:58 +0800

    Initial Code from the boilerplate

---
+++ BuyMeADrink.Web/ClientApp/import-item.d.tsx
@@ -0,0 +1,22 @@
+﻿declare module '*.png'
+
+declare module '*.jpg'
+
+declare module '*.jpeg'
+
+declare module '*.svg'
+
+declare module '*.woff2'
+
+declare module '*.woff'
+
+declare module '*.ttf'
+
+declare module '*.woff?v=4.7.0';
+
+declare module '*.woff2?v=4.7.0';
+
+declare module '*.ttf?v=1.0.0-beta';
+
+
+declare module '*?v=1.0.0-beta';
